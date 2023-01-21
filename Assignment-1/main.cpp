5ac61d9da59670b5c65a6ad5660369bf069dfb0f
Arnav Tuli <arnavtuli00@gmail.com>
2023-01-21 10:41:06 +0530

    Organize examples into separate methods

--- Assignment-1/main.cpp
+++ Assignment-1/main.cpp
@@ -2,6 +2,10 @@
 
 void handleEvents(bool&);
 
+void drawTriangle(SoftwareRasterizer*);
+void drawTick(SoftwareRasterizer*);
+void drawClock(SoftwareRasterizer*);
+
 int main(int argc, char* args[]) {
     int frameWidth = 100, frameHeight = 100;
     int displayScale = 4;
@@ -14,32 +18,10 @@ int main(int argc, char* args[]) {
         softwareRasterizer.turnOnAntiAliasing(25);
         while (!quit) {
             handleEvents(quit);
-            
-            /* draw lecture-2 triangle (red) */
-            // glm::vec4 color(1.0f, 0.0f, 0.0f, 1.0f);
-            // glm::vec4 vertices[3];
-            // vertices[0] = glm::vec4(-0.7f, -0.9f, 0.0f, 1.0f);
-            // vertices[1] = glm::vec4(0.95f, -0.5f, 0.0f, 1.0f);
-            // vertices[2] = glm::vec4(0.32f, 0.81f, 0.0f, 1.0f);
-			// softwareRasterizer.rasterizeTriangle2D(vertices, color);
-            
-            /* draw green tickmark */
-            glm::vec4 vertices[] = {
-                glm::vec4(-0.8,  0.0, 0.0, 1.0),
-                glm::vec4(-0.4, -0.8, 0.0, 1.0),
-                glm::vec4( 0.8,  0.8, 0.0, 1.0),
-                glm::vec4(-0.4, -0.4, 0.0, 1.0)
-            };
-            glm::ivec3 indices[] = {
-                glm::ivec3(0, 1, 3),
-                glm::ivec3(1, 2, 3)
-            };
-            glm::vec4 colors[] = {
-                glm::vec4(0.0, 0.8, 0.0, 1.0),
-                glm::vec4(0.0, 0.8, 0.0, 1.0)
-            };
-            softwareRasterizer.rasterizeArbitraryShape2D(vertices, indices, colors, 2);
-            softwareRasterizer.drawFramebuffer();
+            // Draw red triangle
+            // drawTriangle(&softwareRasterizer);
+            // Draw green tick mark
+            drawTick(&softwareRasterizer);
         }
     }
 	std::string outputFile = "out.png";
@@ -55,6 +37,35 @@ void handleEvents(bool &quit) {
             quit = true;
         }
     }
+}
+
+void drawTriangle(SoftwareRasterizer *pSoftwareRasterizer) {
+    glm::vec4 color(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::vec4 vertices[3];
+    vertices[0] = glm::vec4(-0.7f, -0.9f, 0.0f, 1.0f);
+    vertices[1] = glm::vec4(0.95f, -0.5f, 0.0f, 1.0f);
+    vertices[2] = glm::vec4(0.32f, 0.81f, 0.0f, 1.0f);
+    pSoftwareRasterizer->rasterizeTriangle2D(vertices, color);
+    pSoftwareRasterizer->drawFramebuffer();
+}
+
+void drawTick(SoftwareRasterizer *pSoftwareRasterizer) {
+    glm::vec4 vertices[] = {
+        glm::vec4(-0.8,  0.0, 0.0, 1.0),
+        glm::vec4(-0.4, -0.8, 0.0, 1.0),
+        glm::vec4( 0.8,  0.8, 0.0, 1.0),
+        glm::vec4(-0.4, -0.4, 0.0, 1.0)
+    };
+    glm::ivec3 indices[] = {
+        glm::ivec3(0, 1, 3),
+        glm::ivec3(1, 2, 3)
+    };
+    glm::vec4 colors[] = {
+        glm::vec4(0.0, 0.8, 0.0, 1.0),
+        glm::vec4(0.0, 0.8, 0.0, 1.0)
+    };
+    pSoftwareRasterizer->rasterizeArbitraryShape2D(vertices, indices, colors, 2);
+    pSoftwareRasterizer->drawFramebuffer();
 }
 
 
