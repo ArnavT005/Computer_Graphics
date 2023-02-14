54c01c0ad9f14a57b30d590ac1ae09f1eee42592
Deepanshu Rohilla <deepanshu38841@gmail.com>
2023-02-14 17:26:54 +0530

    clock api done

--- Assignment-1/examples/clock.cpp
+++ Assignment-1/examples/clock.cpp
@@ -1,7 +1,8 @@
 #include "../src/a1.hpp"
+#include "romans.hpp"
 
-// namespace R = COL781::Software;
-namespace R = COL781::Hardware;
+namespace R = COL781::Software;
+// namespace R = COL781::Hardware;
 using namespace glm;
 
 int main() {
@@ -11,87 +12,215 @@ int main() {
     R::ShaderProgram program = r.createShaderProgram(
         r.vsColorTransform(),
         r.fsIdentity()
-    ); 
-
-
-
-
-    // Roman numbers
-    // vec4 unitSquare[] = {
-    //     vec4(-0.5, -0.5, 0.0, 1.0),
-    //     vec4(0.5, -0.5, 0.0, 1.0),
-    //     vec4(0.5, 0.5, 0.0, 1.0),
-    //     vec4(-0.5, 0.5, 0.0, 1.0)
-    // };
-    // ivec3 numberIndices[] = {
-    //     ivec3(0, 1, 2),
-    //     ivec3(2, 3, 0)
-    // };
-    // vec4 numberMarkingColor[] = {
-    //     vec4(0.5, 0.5, 0.5, 1.0),
-    //     vec4(0.5, 0.5, 0.5, 1.0)
-    // };
-
-    // float numberMarkingRadius = 0.70f;
-    // mat4 numberMarkingScale = scale(mat4(1.0f), vec3(0.01, 0.15, 0.15));
-    // R::Object numberShape = r.createObject();
-	// r.setVertexAttribs(numberShape, 0, 4, unitSquare);
-	// r.setVertexAttribs(numberShape, 1, 4, numberMarkingColor);
-	// r.setTriangleIndices(numberShape, 2, numberIndices);
-
-
-
-
-
-
-    // clock forks
-    vec4 forks[] = {
-        vec4(-0.01, -0.3, 0.0, 1.0),
-        vec4(0.01, -0.3, 0.0, 1.0),
-        vec4(0.01, 0.3, 0.0, 1.0),
-        vec4(-0.01, 0.3, 0.0, 1.0)
+    );  
+    vec4 unitSquare[] = {
+            vec4(-0.5, -0.5, 0.0, 1.0),
+            vec4(0.5, -0.5, 0.0, 1.0),
+            vec4(0.5, 0.5, 0.0, 1.0),
+            vec4(-0.5, 0.5, 0.0, 1.0)
     };
-    ivec3 forkIndices[] = {
-        ivec3(0, 1, 3),
-        ivec3(1, 2, 3)
+
+
+    vec4 forkColors[] = {
+		vec4(0.0, 0.0, 0.0, 1.0),
+        vec4(0.0, 0.0, 0.0, 1.0),
+        vec4(0.0, 0.0, 0.0, 1.0),
+        vec4(0.0, 0.0, 0.0, 1.0)
     };
-    vec4 forkMarkingColor[] = {
-        vec4(1.0, 1.0, 1.0, 1.0),
-        vec4(1.0, 1.0, 1.0, 1.0)
+
+    vec4 secondHandColors[] = {
+		vec4(1.0, 0.0, 0.0, 1.0),
+        vec4(1.0, 0.0, 0.0, 1.0),
+        vec4(1.0, 0.0, 0.0, 1.0),
+        vec4(1.0, 0.0, 0.0, 1.0)
     };
-    float forkMarkingRadius = 0.90f;
-    mat4 iScale = scale(mat4(1.0f), vec3(0.1f, 0.125f, 1.0f));
-    mat4 iScaleLarge = scale(mat4(1.0f), vec3(0.25f, 0.25f, 1.0f));
-    R::Object forkShape = r.createObject();
-	r.setVertexAttribs(forkShape, 0, 4, forks);
-	r.setVertexAttribs(forkShape, 1, 4, forkMarkingColor);
-    r.setTriangleIndices(forkShape, 2, forkIndices);
+
+    vec4 MinuteColors[] = {
+		vec4(0.0, 1.0, 0.0, 1.0),
+        vec4(0.0, 1.0, 0.0, 1.0),
+        vec4(0.0, 1.0, 0.0, 1.0),
+        vec4(0.0, 1.0, 0.0, 1.0)
+    };
+
+    vec4 hourHandColors[] = {
+		vec4(0.0, 0.0, 1.0, 1.0),
+        vec4(0.0, 0.0, 1.0, 1.0),
+        vec4(0.0, 0.0, 1.0, 1.0),
+        vec4(0.0, 0.0, 1.0, 1.0)
+    };
+	ivec3 forkTriangles[] = {
+		ivec3(0, 1, 2),
+		ivec3(0, 2, 3)
+	};
+	R::Object fork = r.createObject();
+	r.setVertexAttribs(fork, 0, 4, unitSquare);
+	r.setVertexAttribs(fork, 1, 4, forkColors);
+	r.setTriangleIndices(fork, 2, forkTriangles);
 
 
-        while (!r.shouldQuit()) {
-            r.clear(vec4(1.0, 1.0, 1.0, 1.0));
-            r.useShaderProgram(program);
-            r.setUniform(program, "transform", iScale);
-            r.drawObject(forkShape);
-            r.show();
+    R::Object secondHand = r.createObject();
+    r.setVertexAttribs(secondHand, 0, 4, unitSquare);
+    r.setVertexAttribs(secondHand, 1, 4, secondHandColors);
+    r.setTriangleIndices(secondHand, 2, forkTriangles);
 
-        // for (int i = 0; i < 60; i ++) {
-        //     float markingAngle = radians(i * 6.0f);
-        //     mat4 markingRotate = rotate(mat4(1.0f), markingAngle, vec3(0.0, 0.0, -1.0));
-        //     vec3 markingCenterCoordinate(forkMarkingRadius * sin(markingAngle), forkMarkingRadius * cos(markingAngle), 0.0f);
-        //     mat4 markingTranslate = translate(mat4(1.0f), markingCenterCoordinate);
-        //     if(i%5==0){
-        //         r.setUniform(program, "transform", markingTranslate * markingRotate * iScaleLarge);
-        //         r.drawObject(forkShape);
-        //     }
-        //     else{
-        //         r.setUniform(program, "transform", markingTranslate * markingRotate * iScale);
-        //         r.drawObject(forkShape);
-        //     }
-            
-        // }
+    R::Object minuteHand = r.createObject();
+    r.setVertexAttribs(minuteHand, 0, 4, unitSquare);
+    r.setVertexAttribs(minuteHand, 1, 4, MinuteColors);
+    r.setTriangleIndices(minuteHand, 2, forkTriangles);
+
+    R::Object hourHand = r.createObject();
+    r.setVertexAttribs(hourHand, 0, 4, unitSquare);
+    r.setVertexAttribs(hourHand, 1, 4, hourHandColors);
+    r.setTriangleIndices(hourHand, 2, forkTriangles);
+
+
+    R::Object numbers = r.createObject();
+    r.setVertexAttribs(numbers, 0, 4, unitSquare);
+	r.setVertexAttribs(numbers, 1, 4, forkColors);
+	r.setTriangleIndices(numbers, 2, forkTriangles);
+
+
+
+    mat4 iScale = scale(mat4(1.0f), vec3(0.01f, 0.04f, 1.0f));
+    mat4 iScaleLarge = scale(mat4(1.0f), vec3(0.0125f, 0.125f, 1.0f));
+    mat4 iScaleNumber = scale(mat4(1.0f), vec3(0.01f, 0.15f, 1.0f));
+    float markingRadiusFork = 0.90f;
+    float markingRadiusNumber = 0.7f;
+
+    while (!r.shouldQuit()) {
+        Uint32 displayTime = 0;
+        auto now = std::chrono::system_clock::now();
+        auto timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+        r.clear(vec4(1.0, 1.0, 1.0, 1.0));
+        r.useShaderProgram(program);
+        for (int i = 0; i < 60; i ++) {
+            float markingAngle = radians(i * 6.0f);
+            mat4 markingRotate = rotate(mat4(1.0f), markingAngle, vec3(0.0, 0.0, -1.0));
+            vec3 markingCenterCoordinate(markingRadiusFork * sin(markingAngle), markingRadiusFork * cos(markingAngle), 0.0f);
+            mat4 markingTranslate = translate(mat4(1.0f), markingCenterCoordinate);
+            if(i%5==0){
+                r.setUniform(program, "transform", markingTranslate * markingRotate * iScaleLarge);
+            }
+            else{
+                r.setUniform(program, "transform", markingTranslate * markingRotate * iScale);
+            }
+            r.drawObject(fork);
+        }
+        // I
+        MARK_2D_I(30.0f, 1, 1.05,r);
+        // II
+        MARK_2D_I(60.0f, 2, 1,r);
+        MARK_2D_I(60.0f, 3, 1.05,r);
+
+
+        // III
+        MARK_2D_I(90.0f, 4, 1.0,r);
+        MARK_2D_I(90.0f, 5, 1.05,r);
+        MARK_2D_I(90.0f, 6, 1.10,r);
+
+        // IV
+        MARK_2D_I(120.0f, 7, 1.0,r);
+        MARK_2D_V(120.f, 8, 1, 1.1,r);
+        MARK_2D_V(120.f, 9, -1, 1.1,r);
+        // V
+        MARK_2D_V(150.f, 10, 1, 1.05,r);
+        MARK_2D_V(150.f, 11, -1, 1.05,r);
+
+        // VI
+        MARK_2D_V(180.f, 12, 1, 0.90,r);
+        MARK_2D_V(180.f, 13, -1, 0.90,r);
+        float markingAngle14 = radians(180.0f);
+        vec3 markingCenterCoordinate14(0.05f, markingRadiusNumber * cos(markingAngle14), 0.0f);
+        mat4 markingTranslate14 = translate(mat4(1.0f), markingCenterCoordinate14);
+        r.setUniform(program, "transform", markingTranslate14 * iScaleNumber);
+        r.drawObject(fork);
+
+        // VII
+        MARK_2D_V(210.f, 15, 1, 1.05,r);
+        MARK_2D_V(210.f, 16, -1, 1.05,r);
+        MARK_2D_I(210.0f,17, 0.85,r);
+        MARK_2D_I(210.0f,18, 0.80,r);
+
+
+        // VIII
+        MARK_2D_V(240.f, 19, 1, 1.05,r);
+        MARK_2D_V(240.f, 20, -1, 1.05,r);
+        MARK_2D_I(240.0f,21, 0.95,r);
+        MARK_2D_I(240.0f,22, 0.90,r);
+        MARK_2D_I(240.0f,23, 0.85,r);
+
+        // IX
+        MARK_2D_I(270.0f,24, 1.1,r);
+        MARK_2D_X(270.f, 25, 1, 1.05,r);
+        MARK_2D_X(270.f, 26, -1, 1.05,r);
+
+        // X
+        MARK_2D_X(300.f, 27, 1, 1.05,r);
+        MARK_2D_X(300.f, 28, -1, 1.05,r);
+
+
+        // XI
+        MARK_2D_X(330.f, 29, 1, 1.0,r);
+        MARK_2D_X(330.f, 30, -1, 1.0,r);
+        MARK_2D_I(330.0f,31, 0.90,r);
+
+
+        // XII
+        MARK_2D_X(360.f, 32, 1, 1.0,r);
+        MARK_2D_X(360.f, 33, -1, 1.0,r);
+
+        float markingAngle34 = radians(360.0f);
+        vec3 markingCenterCoordinate34((markingRadiusNumber * sin(markingAngle34)*0.95) + 0.05 , markingRadiusNumber * cos(markingAngle34), 0.0f);
+        mat4 markingTranslate34 = translate(mat4(1.0f), markingCenterCoordinate34);
+        r.setUniform(program, "transform", markingTranslate34 * iScaleNumber);
+        r.drawObject(fork);
+
+        vec3 markingCenterCoordinate35((markingRadiusNumber * sin(markingAngle34)*0.95) + 0.08 , markingRadiusNumber * cos(markingAngle34), 0.0f);
+        mat4 markingTranslate35 = translate(mat4(1.0f), markingCenterCoordinate35);
+        r.setUniform(program, "transform", markingTranslate35 * iScaleNumber);
+        r.drawObject(fork);
+
+
+        Uint32 secondPassed = displayTime % 60;
+        float secondHandRadius = 0.40f;
+        mat4 secondHandScale = scale(mat4(1.0f), vec3(0.025f, 0.8f, 1.0f));
+        float secondHandAngle = radians(secondPassed * 6.0f);
+        mat4 secondHandRotate = rotate(mat4(1.0f), secondHandAngle, vec3(0.0, 0.0, -1.0f));
+        vec3 secondHandCenterCoordinate(secondHandRadius * sin(secondHandAngle), secondHandRadius * cos(secondHandAngle), 0.0f);
+        mat4 secondHandTranslate = translate(mat4(1.0f), secondHandCenterCoordinate);
+        r.setUniform(program, "transform", secondHandTranslate * secondHandRotate * secondHandScale);
+        r.drawObject(secondHand);
+        
+        float minuteHandRadius = 0.30f;
+        mat4 minuteHandScale = scale(mat4(1.0f), vec3(0.05f, 0.6f, 1.0f));
+        float minuteHandAngle = radians(minutePassed * 6.0f);
+        mat4 minuteHandRotate = rotate(mat4(1.0f), minuteHandAngle, vec3(0.0, 0.0, -1.0f));
+        vec3 minuteHandCenterCoordinate(minuteHandRadius * sin(minuteHandAngle), minuteHandRadius * cos(minuteHandAngle), 0.0f);
+        mat4 minuteHandTranslate = translate(mat4(1.0f), minuteHandCenterCoordinate);
+        r.setUniform(program, "transform", minuteHandTranslate * minuteHandRotate * minuteHandScale);
+        r.drawObject(minuteHand);
+
+        float hourPassed = displayTime / 3600.0f - 12 * (displayTime / (12 * 3600));
+        float hourHandRadius = 0.15f;
+        mat4 hourHandScale = scale(mat4(1.0f), vec3(0.075f, 0.3f, 1.0f));
+        float hourHandAngle = radians(hourPassed * 30.0f);
+        mat4 hourHandRotate = rotate(mat4(1.0f), hourHandAngle, vec3(0.0, 0.0, -1.0f));
+        vec3 hourHandCenterCoordinate(hourHandRadius * sin(hourHandAngle), hourHandRadius * cos(hourHandAngle), 0.0f);
+        mat4 hourHandTranslate = translate(mat4(1.0f), hourHandCenterCoordinate);
+        r.setUniform(program, "transform", hourHandTranslate * hourHandRotate * hourHandScale);
+        r.drawObject(hourHand);
+
+
+
+
+
+
+        r.show();
     }
+
+
+
+    
     r.deleteShaderProgram(program);
     return EXIT_SUCCESS;
-	
 }
--- Assignment-1/examples/romans.hpp
+++ Assignment-1/examples/romans.hpp
@@ -4,48 +4,55 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-extern float markingRadius;
-extern glm::mat4 markingScale;
-extern glm::vec3 markingColor;
+#include "../src/a1.hpp"
+
+namespace R = COL781::Software;
+
+
+extern float markingRadiusNumber;
+extern glm::mat4 iScaleNumber;
 extern unsigned int indices[6];
 extern glm::vec2 unitSquare[4];
-// extern class SoftwareRasterizer *pSoftwareRasterizer;
 extern class R::Rasterizer r;
 
-void MARK_2D_I(float angle, int i, float scale);
-void MARK_2D_V(float angle, int i, int j, float scale);
-void MARK_2D_X(float angle, int i, int j, float scale);
+void MARK_2D_I(float angle, int i, float scale, R::Rasterizer &r);
+void MARK_2D_V(float angle, int i, int j, float scale, R::Rasterizer &r);
+void MARK_2D_X(float angle, int i, int j, float scale, R::Rasterizer &r);
 
 #endif
-#define MARK_2D_I(angle,i, scale) \
+#define MARK_2D_I(angle,i, scale, r) \
     float markingAngle##i = glm::radians(angle); \
-    glm::vec3 markingCenterCoordinate##i (markingRadius * glm::sin(markingAngle##i) * scale, \
-                                                            markingRadius * glm::cos(markingAngle##i), \
+    glm::vec3 markingCenterCoordinate##i (markingRadiusNumber * glm::sin(markingAngle##i) * scale, \
+                                                            markingRadiusNumber * glm::cos(markingAngle##i), \
                                                             0.0f); \
     glm::mat4 markingTranslate##i = glm::translate(glm::mat4(1.0f), markingCenterCoordinate##i); \
-    
-    pSoftwareRasterizer->setCustom2d(markingTranslate##i * markingScale); \
-    pSoftwareRasterizer->rasterizeArbitraryShape2D(unitSquare, indices, markingColor, 2);
+    r.setUniform(program, "transform", markingTranslate##i * iScaleNumber);\
+    r.drawObject(fork);
 
 
 
-    #define MARK_2D_V(angle,i,j,scale) \
+
+
+
+
+
+    #define MARK_2D_V(angle,i,j,scale,r) \
     float markingAngle##i = glm::radians(angle); \
-    glm::vec3 markingCenterCoordinate##i ((markingRadius * glm::sin(markingAngle##i) + j*0.0175) * scale, \
-                                                            markingRadius * glm::cos(markingAngle##i), \
-                                                            0.0f); \
-    glm::mat4 markingTranslate##i = glm::translate(glm::mat4(1.0f), markingCenterCoordinate##i); \
-    glm::mat4 markingRotate##i = glm::rotate(glm::mat4(1.0f), glm::radians(j*15.f), glm::vec3(0.0, 0.0, -1.0)); \
-    pSoftwareRasterizer->setCustom2d(markingTranslate##i * markingRotate##i* markingScale); \
-    pSoftwareRasterizer->rasterizeArbitraryShape2D(unitSquare, indices, markingColor, 2);
-
-
-    #define MARK_2D_X(angle,i,j,scale) \
-    float markingAngle##i = glm::radians(angle); \
-    glm::vec3 markingCenterCoordinate##i ((markingRadius * glm::sin(markingAngle##i)) * scale, \
-                                                            markingRadius * glm::cos(markingAngle##i), \
+    glm::vec3 markingCenterCoordinate##i ((markingRadiusNumber * glm::sin(markingAngle##i) + j*0.0175) * scale, \
+                                                            markingRadiusNumber * glm::cos(markingAngle##i), \
                                                             0.0f); \
     glm::mat4 markingTranslate##i = glm::translate(glm::mat4(1.0f), markingCenterCoordinate##i); \
     glm::mat4 markingRotate##i = glm::rotate(glm::mat4(1.0f), glm::radians(j*15.f), glm::vec3(0.0, 0.0, -1.0)); \
-    pSoftwareRasterizer->setCustom2d(markingTranslate##i * markingRotate##i* markingScale); \
-    pSoftwareRasterizer->rasterizeArbitraryShape2D(unitSquare, indices, markingColor, 2);
+    r.setUniform(program, "transform", markingTranslate##i * markingRotate##i* iScaleNumber);\
+    r.drawObject(fork);
+
+
+    #define MARK_2D_X(angle,i,j,scale,r) \
+    float markingAngle##i = glm::radians(angle); \
+    glm::vec3 markingCenterCoordinate##i ((markingRadiusNumber * glm::sin(markingAngle##i)) * scale, \
+                                                            markingRadiusNumber * glm::cos(markingAngle##i), \
+                                                            0.0f); \
+    glm::mat4 markingTranslate##i = glm::translate(glm::mat4(1.0f), markingCenterCoordinate##i); \
+    glm::mat4 markingRotate##i = glm::rotate(glm::mat4(1.0f), glm::radians(j*15.f), glm::vec3(0.0, 0.0, -1.0)); \
+    r.setUniform(program, "transform", markingTranslate##i * markingRotate##i* iScaleNumber);\
+    r.drawObject(fork);
