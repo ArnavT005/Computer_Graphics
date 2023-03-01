f9a17232ecb8ab60878ee6c215527f92ae0bcdfd
Arnav Tuli <arnavtuli00@gmail.com>
2023-03-01 14:30:10 +0530

    Add example files for cube mesh and teapot mesh

---
+++ Assignment-2/examples/cube_mesh.cpp
@@ -0,0 +1,18 @@
+#include "../src/viewer.hpp"
+#include "../src/mesh.hpp"
+
+namespace V = COL781::Viewer;
+namespace M = COL781::Mesh;
+
+using namespace glm;
+
+int main() {
+    M::Mesh m;
+	V::Viewer v;
+	if (!v.initialize("Square Mesh", 640, 480)) {
+		return EXIT_FAILURE;
+	}
+    m.load("../meshes/cube.obj");
+    m.send(v);
+	v.view();
+}
