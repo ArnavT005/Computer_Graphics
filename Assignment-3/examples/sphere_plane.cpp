f16ded032a9aae4ffc66bf2513389832b1c8b7d6
Arnav Tuli <arnavtuli00@gmail.com>
2023-04-10 23:39:15 +0530

    Complete scene 2

--- Assignment-3/examples/sphere_plane.cpp
+++ Assignment-3/examples/sphere_plane.cpp
@@ -15,9 +15,16 @@ int main() {
     DiffusePlane p;
     p.setPoint(glm::vec3(0.0f, -1.0f, 0.0f));
     p.setNormal(glm::vec3(0.0f, 1.0f, 0.0f));
+    DiffuseBox b[3];
+    b[0].setPoints(glm::vec3(-2.5, -1, -3), glm::vec3(-1.5, 2, -1));
+    b[1].setPoints(glm::vec3(1.5, -1, -2), glm::vec3(3.5, 0, -1));
+    b[2].setPoints(glm::vec3(-1.5, -1, -5), glm::vec3(4, 2, -3.5));
     r.addObject(&s);
     r.addObject(&p);
-    r.calibrateCamera(90.0f, 1.0f, glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    r.addObject(&b[0]);
+    r.addObject(&b[1]);
+    r.addObject(&b[2]);
+    r.calibrateCamera(60.0f, 1.0f, glm::vec3(0, 0, 4), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     r.clearBuffer(glm::vec4(1.0f));
     r.traceRays();
     return EXIT_SUCCESS;
